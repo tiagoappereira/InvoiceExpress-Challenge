96cf08e7bb04ed68d3d75e4cff16e65cdd48c56b
tiagoappereira <alex.tiago.pereira@hotmail.com>
2019-12-15 21:27:17 +0000

    remove build

--- build/resources/test/createPosts.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO post(id, description, title, post_user) VALUES (1, 'p1', 'p1', 1);
-INSERT INTO post(id, description, title, post_user) VALUES (2, 'p2', 'p2', 1);
-INSERT INTO post(id, description, title, post_user) VALUES (3, 'p3', 'p3', 1);
-INSERT INTO post(id, description, title, post_user) VALUES (4, 'p4', 'p4', 1);
-INSERT INTO post(id, description, title, post_user) VALUES (5, 'p5', 'p5', 1);
--- build/resources/test/createUsers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO User_ (id, password, username) VALUES (1, 'admin', 'admin');
-INSERT INTO User_ (id, password, username) VALUES (2, 'u1', 'u1');
-INSERT INTO User_ (id, password, username) VALUES (3, 'u2', 'u2');
-INSERT INTO User_ (id, password, username) VALUES (4, 'u3', 'u3');
-INSERT INTO User_ (id, password, username) VALUES (5, 'u4', 'u4');
-INSERT INTO User_ (id, password, username) VALUES (6, 'u5', 'u5');
